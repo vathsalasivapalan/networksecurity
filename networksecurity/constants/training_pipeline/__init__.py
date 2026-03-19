6e35c5c3e3cecdcba6b0a9924c0e549b3cbd3dba
vathsala <vathsalasivapalan@gmail.com>
2026-03-19 17:25:51 +0100

    Data Transformation components is ready

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -40,6 +40,8 @@ DATA_VALIDATION_VALID_DIR: str = "validated"
 DATA_VALIDATION_INVALID_DIR: str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+
 PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
 """
